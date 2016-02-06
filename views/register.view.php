e0ec2eed5338137f306b4227a2fc07962396bb54
Mouadh Kahouli <mouadh.kahouli.isi@gmail.com>
2016-02-06 22:47:01 +0100

    static form of register static form of register

--- views/register.view.php
+++ views/register.view.php
@@ -5,13 +5,35 @@
 
 <div id="main-content">
     <div class="container">
-        <div class="jumbotron">
-            <h1>Devenez dès présent membre:</h1>
-
-            <form action="" method="post">
-
-            </form>
-        </div>
+        <h1>Devenez dès présent membre:</h1>
+        <form action="" method="post" class="well col-md-6" >
+            <!-- Name field -->
+            <div class="form-group">
+                <label class="control-label" for="name">Nom:</label>
+                <input type="text" class="form-control" id="name" name="name" required="required">
+            </div>
+            <!-- Pseudo field -->
+            <div class="form-group">
+                <label class="control-label" for="pseudo">Pseudo:</label>
+                <input type="text" class="form-control" id="pseudo" name="pseudo" required="required">
+            </div>
+            <!-- Email field -->
+            <div class="form-group">
+                <label class="control-label" for="email">Email:</label>
+                <input type="email" class="form-control" id="email" name="email" required="required">
+            </div>
+            <!-- Password field -->
+            <div class="form-group">
+                <label class="control-label" for="password">Mot de passe:</label>
+                <input type="password" class="form-control" id="password" name="password" required="required">
+            </div>
+            <!-- Password confirmation field -->
+            <div class="form-group">
+                <label class="control-label" for="password_confirm">Confirmer mot de passe:</label>
+                <input type="password" class="form-control" id="password_confirm" name="password_confirm" required="required">
+            </div>
+            <input type="submit" class="btn btn-primary" value="Inscription" name="register">
+        </form>
     </div><!-- /.container -->
 </div>
 
