f02fdb2b493fc5468c6663d6953f1495b28e5fbc
Mouadh Kahouli <mouadh.kahouli.isi@gmail.com>
2016-02-06 23:46:49 +0100

    empty function to validate registration

--- register.php
+++ register.php
@@ -1,3 +1,38 @@
 <?php
+    //si le formulaire a été soumis
+    if(isset($_POST['register'])){
+        //si tous les champs ont été remplis
+        if(!empty($_POST['name']) && !empty($_POST['pseudo']) && !empty($_POST['email']) && !empty($_POST['password'])
+            && !empty($_POST['password_confirm'])){
+            $errors = []; //tab centenant les erreurs
+            extract($_POST);
 
-require('views/register.view.php');
+            if(mb_strlen($pseudo) < 3){
+                $errors[] = "le pseudo doit contenir 3 caractères au minimum";
+            }
+            if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+                $errors[] = "adresse mail invalide";
+            }
+            if(mb_strlen($password) < 6){
+                $errors[] = "le mot de passe doit contenir 6 caractères au minimum";
+            }else{
+                if($password != $password_confirm){
+                    $errors[] = "mots de passe non conformes";
+                }
+            }
+            if(is_already_in_use('pseudo', $pseudo, 'users')){
+                $errors[] = "Pseudo déjà utilisé";
+            }
+            if(is_already_in_use('email', $email, 'users')){
+                $errors[] = "Adresse email déjà utilisée";
+            }
+            if(count($errors) == 0){
+                //envoyer mail d'activation
+            }
+        }else{
+            $errors[] = "Veuillez remplir tous les champs!";
+        }
+    }
+?>
+
+<?php require('views/register.view.php'); ?>
