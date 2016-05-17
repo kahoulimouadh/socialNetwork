0183f38f8a7e6c13cd05395d32bc423802c0b74c
mouadh <mouadh.kahouli14@gmail.com>
2016-05-17 16:33:26 +0100

    add form

--- views/profile.view.php
+++ views/profile.view.php
@@ -32,16 +32,72 @@
                         <input type="text" name="name" id="name" class="form-control" placeholder="john" required="required">
                     </div>
                   </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="city">Ville <span class="text-danger">*</span></label>
+                        <input type="text" name="city" id="city" class="form-control" required="required">
+                    </div>
+                  </div>
                 </div>
 
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="country">Pays <span class="text-danger">*</span></label>
+                        <input type="text" name="country" id="country" class="form-control" required="required">
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="sex">Sexe <span class="text-danger">*</span></label>
+                        <select class="form-control" name="sex" id="sex">
+                          <option value="H">Homme</option>
+                          <option value="F">Femme</option>
+                        </select>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="twitter">Twitter</label>
+                        <input type="text" name="twitter" id="twitter" class="form-control">
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="github">Github</label>
+                      <input type="text" name="github" id="github" class="form-control">
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="form-group">
+                        <label for="available_fot_hire">
+                          <input type="checkbox" name="available_fot_hire" id="available_fot_hire"/>
+                          Disponible pour emploi?
+                        </label>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="form-group">
+                        <label for="bio">Biographie<span class="text-danger">*</span></label>
+                        <textarea name="bio" id="bio" rows="6" cols="40" class="form-control" placeholder="j'aime la programmation..."></textarea>
+                    </div>
+                  </div>
+                </div>
+                <input type="submit" class="btn btn-primary" name="update" value="Valider">
               </form>
             </div>
           </div>
         </div>
       </div>
-
-
-
     </div><!-- /.container -->
 </div>
 
